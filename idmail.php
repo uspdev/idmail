8a6362a47fd96320fa222fc69cd4b8dceddd9cd0
Will <wgnann@gmail.com>
2019-06-19 17:14:35 -0300

    IDMail() é chamado depois de verificar argv

--- idmail.php
+++ idmail.php
@@ -2,11 +2,11 @@
 require 'vendor/autoload.php';
 require 'IDMail.php';
 
-$idmail = new IDMail();
-
 if (!isset($argv[1])) {
     die("uso: ".$argv[0]." [nusp]\n");
 }
+
+$idmail = new IDMail();
 
 $json = json_decode($idmail->id_get_emails($argv[1]));
 echo $idmail->extract_email($json)."\n";
